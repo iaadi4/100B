89a929770a72e9742f0b9841083835825698d778
Aditya Singh <adityasingh40675@gmail.com>
2024-10-27 21:17:53 +0530

    feat: Add logout method

--- server/src/routes/v1/index.ts
+++ server/src/routes/v1/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import authController from "../../controllers/authController";
 import handleRefreshToken from "../../controllers/refreshTokenController";
+import handleLogout from "../../controllers/logoutController";
 
 const { signup, login } = authController;
 
@@ -9,5 +10,6 @@ const router: any = express.Router();
 router.post('/signup', signup);
 router.post('/login', login);
 router.get('/refresh', handleRefreshToken);
+router.get('/logout', handleLogout);
 
 export default router;
