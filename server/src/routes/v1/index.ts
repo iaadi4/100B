ae739c62ae6b39bee368edbcf120ac1f88809d97
Aditya Singh <adityasingh40675@gmail.com>
2024-10-29 17:37:45 +0530

    feat: Add constraints on file upload, Improve multer error handling

--- server/src/routes/v1/index.ts
+++ server/src/routes/v1/index.ts
@@ -1,5 +1,5 @@
-import express from "express";
-import multer from "multer";
+import express, { NextFunction, Request, Response } from "express";
+import multer, { MulterError, FileFilterCallback } from "multer";
 import authController from "../../controllers/authController";
 import handleRefreshToken from "../../controllers/refreshTokenController";
 import handleLogout from "../../controllers/logoutController";
@@ -8,13 +8,53 @@ import { verifyJwt } from "../../middlewares/verifyJwt";
 
 const { signup, login } = authController;
 
+enum statusCode {
+    NOT_FOUND = 404,
+    BAD_REQUEST = 400,
+    INTERNAL_ERROR = 500
+}
+
 const router: any = express.Router();
-const uploadHandle = multer({ storage: multer.memoryStorage() });
+const storage = multer.memoryStorage();
+
+const fileFilter = (req: Request, file: any, cb: FileFilterCallback) => {
+    if(file.mimetype.split('/')[0] == "application" || file.mimetype.split('/')[0] == "image") {
+        cb(null, true);
+    } else {
+        //@ts-ignore
+        cb(new MulterError('LIMIT_UNEXPECTED_FILE'), false);
+    }
+}
+
+const uploadHandle = multer({
+    storage,
+    fileFilter,
+    limits: { fileSize: 50000000, files: 1}
+})
 
 router.post('/signup', signup);
 router.post('/login', login);
 router.get('/refresh', handleRefreshToken);
 router.get('/logout', handleLogout);
 router.post('/upload', verifyJwt, uploadHandle.single('file'), upload);
+
+router.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if(err instanceof MulterError) {
+        if(err.code == "LIMIT_UNEXPECTED_FILE") {
+            return res.status(statusCode.BAD_REQUEST).json({
+                error: "File type not supported"
+            })
+        } else if(err.code == "LIMIT_FILE_SIZE") {
+            return res.status(statusCode.BAD_REQUEST).json({
+                error: "File size too large"
+            })
+        } else if(err.code == "LIMIT_FILE_COUNT") {
+            return res.status(statusCode.BAD_REQUEST).json({
+                error: "Too many files"
+            })
+        }
+    }
+    next();
+})
 
 export default router;
