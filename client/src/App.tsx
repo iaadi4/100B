eb71b14dfa565cfc61db548b6102aa323dd10061
Aditya Singh <adityasingh40675@gmail.com>
2024-11-05 18:39:47 +0530

    feat: Add redux persist

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import NotFoundPage from './Pages/Error/Error';
 import Signup from "./Pages/Auth/Signup";
@@ -6,16 +6,19 @@ import Login from './Pages/Auth/Login';
 import ForgotPassword from './Pages/Auth/ForgotPassword';
 import ResetPassword from './Pages/Auth/ResetPassword';
 import EmailVerificationPage from './Pages/Auth/EmailVerify';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const user = useSelector((state: any) => state.auth.userData);
+
   return (
     <BrowserRouter >
       <Routes>
         <Route path='*' element={<NotFoundPage />} />
-        <Route path='/' element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path='/' element={ user? <Home /> : <Navigate to={'/login'} />} />
+        <Route path="/signup" element={ user? <Navigate to={'/'} /> : <Signup />} />
+        <Route path="/login" element={ user? <Navigate to={'/'} /> : <Login />} />
+        <Route path="/forgot-password" element={ <ForgotPassword /> } />
         <Route path="/verify-email" element={<EmailVerificationPage />} />
         <Route path='/reset-password/:token' element={<ResetPassword />} />
       </Routes>
