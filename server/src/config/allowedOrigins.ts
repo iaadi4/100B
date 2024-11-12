39a1bf3efda33a32f544c32ccfd4e89e5e0a3226
Aditya Singh <adityasingh40675@gmail.com>
2024-11-12 21:59:07 +0530

    fix: cors error

--- server/src/config/allowedOrigins.ts
+++ server/src/config/allowedOrigins.ts
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://127.0.0.1:5173',
     'http://localhost:5173',
-    'https://100b-orpin.vercel.app'
+    'https://100b-lilac.vercel.app'
 ];
 
 export default allowedOrigins;
