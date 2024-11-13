e0e5c838da49c33c6c04141731f96bbb0a745bcd
Aditya Singh <adityasingh40675@gmail.com>
2024-11-13 11:38:34 +0530

    feat: Add tooltip in sidebar

--- client/src/components/SidebarMenu.tsx
+++ client/src/components/SidebarMenu.tsx
@@ -5,10 +5,18 @@ import { RiStickyNote2Fill2 } from "react-icons/ri";
 import { PiLockSimpleFill } from "react-icons/pi";
 import { IoNotifications } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
-import { RiLogoutBoxFill } from "react-icons/ri";
+import { RiLoginBoxFill } from "react-icons/ri";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { logout } from "@/Redux/authSlice";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+
 
 const SidebarMenu = () => {
   const navigate = useNavigate();
@@ -20,48 +28,108 @@ const SidebarMenu = () => {
   };
 
   return (
-    <div className="flex justify-center h-screen w-[5%] min-w-[65px]">
-      <div className="flex items-center flex-col h-full w-full">
-        <NavLink
-          to="/"
-          className={({ isActive }) => `h-10 w-10 flex justify-center items-center p-2 cursor-pointer rounded-lg mt-6 ${isActive ? 'bg-orange-500' : ''}`}>
-          <GoHomeFill className="w-5 h-5" />
-        </NavLink>
-        <NavLink
-          to="/notes"
-          className={({ isActive }) => `h-10 w-10 flex justify-center items-center p-2 cursor-pointer rounded-lg mt-3 ${isActive ? 'bg-orange-500' : ''}`}>
-          <RiStickyNote2Fill2 className="w-5 h-5" />
-        </NavLink>
-        <NavLink
-          to="/chats"
-          className={({ isActive }) => `h-10 w-10 flex justify-center items-center p-2 cursor-pointer rounded-lg mt-3 ${isActive ? 'bg-orange-500' : ''}`}>
-          <IoChatbubble className="w-5 h-5" />
-        </NavLink>
-        <NavLink
-          to="/polls"
-          className={({ isActive }) => `h-10 w-10 flex justify-center items-center p-2 cursor-pointer rounded-lg mt-3 ${isActive ? 'bg-orange-500' : ''}`}>
-          <FaPoll className="w-5 h-5" />
-        </NavLink>
-        <NavLink
-          to="/confession"
-          className={({ isActive }) => `h-10 w-10 flex justify-center items-center p-2 cursor-pointer rounded-lg mt-3 ${isActive ? 'bg-orange-500' : ''}`}>
-          <PiLockSimpleFill className="w-5 h-5" />
-        </NavLink>
-        <NavLink
-          to="/announcements"
-          className={({ isActive }) => `h-10 w-10 flex justify-center items-center p-2 cursor-pointer rounded-lg mt-3 ${isActive ? 'bg-orange-500' : ''}`}>
-          <IoNotifications className="w-5 h-5" />
-        </NavLink>
-        <NavLink
-          to="/profile"
-          className={({ isActive }) => `h-10 w-10 mb-6 flex justify-center items-center p-2 cursor-pointer rounded-lg mt-3 ${isActive ? 'bg-orange-500' : ''}`}>
-          <FaUser className="w-5 h-5" />
-        </NavLink>
-        <div onClick={handleLogout} className="h-10 w-10 mb-6 mt-auto flex justify-center items-center p-2 hover:bg-orange-500 cursor-pointer rounded-lg">
-          <RiLogoutBoxFill className="w-5 h-5" />
+    <TooltipProvider>
+      <div className="flex justify-center h-screen w-[5%] min-w-[65px]">
+        <div className="flex items-center flex-col h-full w-full">
+          <Tooltip>
+            <TooltipTrigger>
+              <NavLink
+                to="/"
+                className={({ isActive }) => `h-10 w-10 flex justify-center items-center p-2 cursor-pointer rounded-lg mt-6 ${isActive ? 'bg-orange-500 text-white' : ''}`}>
+                <GoHomeFill className="w-5 h-5" />
+              </NavLink>
+            </TooltipTrigger>
+            <TooltipContent side="right">
+              <p>Home</p>
+            </TooltipContent>
+          </Tooltip>
+          <Tooltip>
+            <TooltipTrigger>
+              <NavLink
+                to="/notes"
+                className={({ isActive }) => `h-10 w-10 flex justify-center items-center p-2 cursor-pointer rounded-lg mt-3 ${isActive ? 'bg-orange-500 text-white' : ''}`}>
+                <RiStickyNote2Fill2 className="w-5 h-5" />
+              </NavLink>
+            </TooltipTrigger>
+            <TooltipContent side="right">
+              <p>Notes</p>
+            </TooltipContent>
+          </Tooltip>
+          <Tooltip>
+            <TooltipTrigger>
+              <NavLink
+                to="/chats"
+                className={({ isActive }) => `h-10 w-10 flex justify-center items-center p-2 cursor-pointer rounded-lg mt-3 ${isActive ? 'bg-orange-500 text-white' : ''}`}>
+                <IoChatbubble className="w-5 h-5" />
+              </NavLink>
+            </TooltipTrigger>
+            <TooltipContent side="right">
+              <p>Chats</p>
+            </TooltipContent>
+          </Tooltip>
+          <Tooltip>
+            <TooltipTrigger>
+              <NavLink
+                to="/polls"
+                className={({ isActive }) => `h-10 w-10 flex justify-center items-center p-2 cursor-pointer rounded-lg mt-3 ${isActive ? 'bg-orange-500 text-white' : ''}`}>
+                <FaPoll className="w-5 h-5" />
+              </NavLink>
+            </TooltipTrigger>
+            <TooltipContent side="right">
+              <p>Polls</p>
+            </TooltipContent>
+          </Tooltip>
+          <Tooltip>
+            <TooltipTrigger>
+              <NavLink
+                to="/confession"
+                className={({ isActive }) => `h-10 w-10 flex justify-center items-center p-2 cursor-pointer rounded-lg mt-3 ${isActive ? 'bg-orange-500 text-white' : ''}`}>
+                <PiLockSimpleFill className="w-5 h-5" />
+              </NavLink>
+            </TooltipTrigger>
+            <TooltipContent side="right">
+              <p>Confessions</p>
+            </TooltipContent>
+          </Tooltip>
+          <Tooltip>
+            <TooltipTrigger>
+              <NavLink
+                to="/announcements"
+                className={({ isActive }) => `h-10 w-10 flex justify-center items-center p-2 cursor-pointer rounded-lg mt-3 ${isActive ? 'bg-orange-500 text-white' : ''}`}>
+                <IoNotifications className="w-5 h-5" />
+              </NavLink>
+            </TooltipTrigger>
+            <TooltipContent side="right">
+              <p>Announcements</p>
+            </TooltipContent>
+          </Tooltip>
+          <Tooltip>
+            <TooltipTrigger>
+              <NavLink
+                to="/profile"
+                className={({ isActive }) => `h-10 w-10 mb-6 flex justify-center items-center p-2 cursor-pointer rounded-lg mt-3 ${isActive ? 'bg-orange-500 text-white' : ''}`}>
+                <FaUser className="w-5 h-5" />
+              </NavLink>
+            </TooltipTrigger>
+            <TooltipContent side="right">
+              <p>Profile</p>
+            </TooltipContent>
+          </Tooltip>
+          <div className="mt-auto flex">
+            <Tooltip>
+              <TooltipTrigger>
+                <div onClick={handleLogout} className="h-10 w-10 mb-6 flex justify-center items-center p-2 hover:bg-orange-500 hover:text-white cursor-pointer rounded-lg">
+                  <RiLoginBoxFill className="w-5 h-5" />
+                </div>
+              </TooltipTrigger>
+              <TooltipContent className="mb-8" side="right">
+                <p>Sign out</p>
+              </TooltipContent>
+            </Tooltip>
+          </div>
         </div>
       </div>
-    </div>
+    </TooltipProvider>
   );
 };
 
