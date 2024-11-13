0bc77865d24de498be73bdaf1fa85f1df3f71f1c
Aditya Singh <adityasingh40675@gmail.com>
2024-11-13 14:03:51 +0530

    feat: Add method to get polls by different filter

--- server/src/services/pollService.ts
+++ server/src/services/pollService.ts
@@ -2,21 +2,26 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+interface Ifilters {
+    year?: string
+    branch?: string
+}
+
 class PollService {
     async create(data: any, userId: number) {
         try {
-            if(data.options.length > 4)
+            if (data.options.length > 4)
                 throw new Error('Max 4 options allowed');
             let response;
-            if(!data.closesAt) {
+            if (!data.closesAt) {
                 const closesAt = new Date();
                 closesAt.setHours(closesAt.getHours() + 24);
                 response = await prisma.poll.create({
-                    data: {...data, userId, closesAt}
+                    data: { ...data, userId, closesAt }
                 })
             } else {
                 response = await prisma.poll.create({
-                    data: {...data, userId}
+                    data: { ...data, userId }
                 })
             }
             return response;
@@ -67,6 +72,85 @@ class PollService {
                     closesAt
                 }
             })
+        } catch (error) {
+            console.log('Something went wrong in the service layer');
+            throw error;
+        }
+    }
+
+    async getPoll(pollId: string) {
+        try {
+            const poll = await prisma.poll.findFirst({
+                where: {
+                    id: parseInt(pollId)
+                }
+            })
+            return poll;
+        } catch (error) {
+            console.log('Something went wrong in the service layer');
+            throw error;
+        }
+    }
+
+    async getPollsByDateCreation(pageNo: string, ascending: string) {
+        try {
+            const polls = await prisma.poll.findMany({
+                skip: (parseInt(pageNo) - 1) * 4,
+                take: 4,
+                orderBy: {
+                    createdAt: ascending === 'true' ? 'asc' : 'desc'
+                }
+            })
+            return polls;
+        } catch (error) {
+            console.log('Something went wrong in the service layer');
+            throw error;
+        }
+    }
+
+    async getPollByYearOrBranch(pageNo: string, year?: string, branch?: string) {
+        try {
+            const filters: Ifilters = {};
+            if (year) filters.year = year;
+            if (branch) filters.branch = branch;
+            const polls = await prisma.poll.findMany({
+                skip: (parseInt(pageNo) - 1) * 4,
+                take: 4,
+                where: filters
+            })
+            return polls;
+        } catch (error) {
+            console.log('Something went wrong in the service layer');
+            throw error;
+        }
+    }
+
+    async getPolls(pageNo: string) {
+        try {
+            const polls = await prisma.poll.findMany({
+                skip: (parseInt(pageNo) - 1) * 4,
+                take: 4
+            });
+            return polls;
+        } catch (error) {
+            console.log('Something went wrong in the service layer');
+            throw error;
+        }
+    }
+
+    async getPollsByTitle(pageNo: string, searchTitle: string) {
+        try {
+            const polls = await prisma.poll.findMany({
+                skip: (parseInt(pageNo) - 1) * 4,
+                take: 4,
+                where: {
+                    title: {
+                        contains: searchTitle,
+                        mode: 'insensitive'
+                    }
+                }
+            })
+            return polls;
         } catch (error) {
             console.log('Something went wrong in the service layer');
             throw error;
